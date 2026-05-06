d1ce19534e5c8a97c595be6c029a1f955960d45d
Sang Young Noh <sangyoung123@gmail.com>
2026-05-06 20:52:04 +0100

    fix: new

--- src/molecule/mol_pxr_challenge_train.py
+++ src/molecule/mol_pxr_challenge_train.py
@@ -35,6 +35,16 @@ from mol_models import GINELayerUpgraded
 BATCH_SIZE = 64
 NUM_WORKERS = 4
 
+def attach_targets(df: pd.DataFrame, target_col: str = "pEC50") -> pd.DataFrame:
+    df = df.copy().reset_index(drop = True)
+    df["graph"] = df["graph"].astype(object)
+
+    for idx, graph in df["graph"].items():
+        y_value = df.at[idx, target_col]
+        graph.y = torch.tensor([y_value], dtype = torch.float32)
+
+    return df
+    
 def advanced_smiles_to_pyg_data(smiles: str) -> Data:
     """
     Build a PyG Data object from ``advanced_smiles_to_graph`` outputs.
@@ -95,7 +105,7 @@ def make_loaders(
     }
     if num_workers > 0:
         loader_kwargs["persistent_workers"] = True
-        loader_kwargs["prefetch_factor"] = 2
+        loader_kwargs["prefetch_factor"] = 2#
 
     train_loader = DataLoader(df_train["graph"].tolist(), shuffle=True, **loader_kwargs)
     val_loader = DataLoader(df_val["graph"].tolist(), shuffle=False, **loader_kwargs)
@@ -118,30 +128,32 @@ def make_loaders(
 ## Single-concentration config
 #ds_single = load_dataset("openadmet/pxr-challenge-train-test", "single_concentration")
 #train_single = ds_single["train"]
-
-
-def main() -> None:
-
     
-    SEED = 42
+SEED = 42
 
-    train         = pd.read_csv("hf://datasets/openadmet/pxr-challenge-train-test/pxr-challenge_TRAIN.csv")
-    test          = pd.read_csv("hf://datasets/openadmet/pxr-challenge-train-test/pxr-challenge_TEST_BLINDED.csv")
-    train_counter  = pd.read_csv("hf://datasets/openadmet/pxr-challenge-train-test/pxr-challenge_counter-assay_TRAIN.csv")
-    test_structure = pd.read_csv("hf://datasets/openadmet/pxr-challenge-train-test/pxr-challenge_structure_TEST_BLINDED.csv")
-    train_single   = pd.read_csv("hf://datasets/openadmet/pxr-challenge-train-test/pxr-challenge_single_concentration_TRAIN.csv")
+train         = pd.read_csv("hf://datasets/openadmet/pxr-challenge-train-test/pxr-challenge_TRAIN.csv")
+test          = pd.read_csv("hf://datasets/openadmet/pxr-challenge-train-test/pxr-challenge_TEST_BLINDED.csv")
+train_counter  = pd.read_csv("hf://datasets/openadmet/pxr-challenge-train-test/pxr-challenge_counter-assay_TRAIN.csv")
+test_structure = pd.read_csv("hf://datasets/openadmet/pxr-challenge-train-test/pxr-challenge_structure_TEST_BLINDED.csv")
+train_single   = pd.read_csv("hf://datasets/openadmet/pxr-challenge-train-test/pxr-challenge_single_concentration_TRAIN.csv")
 
-    train['graph'] = train['SMILES'].apply(advanced_smiles_to_pyg_data) # convert the SMILES strings to PyG Data objects and store them in a new column 'graph'
-    u_train = np.stack([rdkit_globals(s) for s in train["SMILES"]], axis=0) # glo
-    u_scaler = StandardScaler().fit(u_train) # scale the global feature for each molecule
+train['graph'] = train['SMILES'].apply(advanced_smiles_to_pyg_data) # convert the SMILES strings to PyG Data objects and store them in a new column 'graph'
 
-    df_train, df_val, df_test = split_df(train, train=0.8, val=0.1, seed=SEED) # split the data into train, validation and test sets 
+df_train, df_val, df_test = split_df(train, train=0.8, val=0.1, seed=SEED)
+
+u_train = np.stack([rdkit_globals(s) for s in df_train["SMILES"]], axis=0)
+u_scaler = StandardScaler().fit(u_train)
      
-    # attach 
-    df_train = attach_u_features(df_train, u_scaler) # attach the global features to the graph objects in the dataframe 
-    df_val = attach_u_features(df_val, u_scaler) # attach the global features to the graph objects in the dataframe 
-    df_test = attach_u_features(df_test, u_scaler)
-    
-    train_loader, val_loader, test_loader = make_loaders(df_train, df_val, df_test)
+# attach 
+df_train = attach_u_features(df_train, u_scaler) # attach the global features to the graph objects in the dataframe 
+df_val = attach_u_features(df_val, u_scaler) # attach the global features to the graph objects in the dataframe 
+df_test = attach_u_features(df_test, u_scaler)
+
+# attach the target values to the graph objects in the dataframe, so that they can be easily accessed during training and evaluation
+df_train = attach_targets(df_train)
+df_val = attach_targets(df_val)
+df_test = attach_targets(df_test)
+
+train_loader, val_loader, test_loader = make_loaders(df_train, df_val, df_test)
 
     
